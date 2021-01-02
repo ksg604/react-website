540b3f9bd647844b269028360202db8d43ab9e1b
Kevin San Gabriel <kevinsangabriel@Kevins-MacBook-Pro-2.local>
2021-01-01 22:29:38 -0800

    addadded states

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Menu from './Menu';
-
-function Paragraph() {
-  return(<p>fuck dog</p>)
-}
+import Menu from './components/Menu';
+import Home from './components/Home';
+import {useState} from 'react';
 
 function App() {
-  const paragraph = <p>hello</p>
+  const displayComponent = (<div></div>);
+  const [currentPage, setCurrentPage] = useState("home");
+  const onNavigation = (page) => { 
+    
+    console.log(page);
+    setCurrentPage(page) };
+
+  const getCurrentPage = () => {
+    switch (currentPage) { 
+      case "home":
+        return <Home/>
+      case "project":
+        return <div>Project</div>
+      case "portfolio":
+        return <div>Portfolio</div>
+      }
+    } 
+
   return (
     <div className="App">
-      <Menu/>
+      <Menu onNavigation={onNavigation} />
       <div className="Title">
         <h1>Kevin's Website</h1>
+        {getCurrentPage()}
       </div>
     </div>
   );
